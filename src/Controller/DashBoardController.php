8b4b0a65c0353a2ccbcad59f2738c312fb3d9b73
Arthur serenne <arthur.serenne@gmail.com>
2024-05-15 14:57:41 +0200

    Ajout du dashboard front et page de navigation

--- src/Controller/DashBoardController.php
+++ src/Controller/DashBoardController.php
@@ -5,14 +5,31 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
 
 class DashBoardController extends AbstractController
 {
-    #[Route('/', name: 'app_dashboard')]
-    public function index(): Response
+    #[Route('/dashboard', name: 'app_dashboard')]
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashBoardController',
+        return $this->render('dashboard/base.html.twig', [
+            'user' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/dashboard/test', name: 'app_dashboard_test')]
+    public function test(UserRepository $userRepository): Response
+    {
+        return $this->render('dashboard/test.html.twig', [
+            'user' => $userRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/dashboard/components', name: 'app_dashboard_components')]
+    public function components(UserRepository $userRepository): Response
+    {
+        return $this->render('dashboard/components.html.twig', [
+            'user' => $userRepository->findAll(),
         ]);
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        customColor : '#FF00EE',
+        'primary': {
+          '50': '#fffaec',
+          '100': '#fff5d3',
+          '200': '#ffe7a5',
+          '300': '#ffd56d',
+          '400': '#ffb632',
+          '500': '#ff9e0a',
+          '600': '#f48000',
+          '700': '#cc6202',
+          '800': '#a14b0b',
+          '900': '#82400c',
+          '950': '#461e04',
+        },
+      }
+    },
   },
   plugins: [],
 }
